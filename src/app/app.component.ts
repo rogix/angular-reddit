92c8000558d85d9ec77440bfa74bbd6194b5a17e
root <roggeriomarx@gmail.com>
2017-08-16 08:41:40 -0300

    adding new articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Article } from './article/article.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+  articles: Article[];
+
+  constructor() { 
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fulstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
   }
 }
